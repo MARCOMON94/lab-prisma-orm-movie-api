0dc6e621b299c545103c00ec93b931639522b97a
MARCO <125563435+MARCOMON94@users.noreply.github.com>
2026-05-13 23:04:25 +0100

    .

--- src/routes/peliculas.js
+++ src/routes/peliculas.js
@@ -1,5 +1,8 @@
 const { Router } = require('express')
 
+const verificarToken = require('../middleware/verificarToken')
+const verificarRol = require('../middleware/verificarRol')
+
 const {
   listarPeliculas,
   obtenerPelicula,
@@ -8,20 +11,17 @@ const {
   eliminarPelicula,
   listarResenas,
   crearResena
-} = require('../controllers/peliculasController')
-
-const verificarToken = require('../middleware/verificarToken')
-const verificarRol = require('../middleware/verificarRol')
+} = require('../controllers/peliculasPrismaController')
 
 const router = Router()
+
+router.get('/', listarPeliculas)
 
 router.get('/:id/resenas', listarResenas)
 router.post('/:id/resenas', verificarToken, crearResena)
 
-router.get('/', listarPeliculas)
 router.get('/:id', obtenerPelicula)
-
-router.post('/', verificarToken, crearPelicula)
+router.post('/', verificarToken, verificarRol('admin'), crearPelicula)
 router.put('/:id', verificarToken, verificarRol('admin'), actualizarPelicula)
 router.delete('/:id', verificarToken, verificarRol('admin'), eliminarPelicula)
 
